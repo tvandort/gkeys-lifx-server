47d4088d779b3d3fe874be352909e961926b2986
tvand <tvandort1@gmail.com>
2018-01-30 22:12:26 -0500

    Correct api usage.

--- src/lights/Commands.ts
+++ src/lights/Commands.ts
@@ -2,6 +2,6 @@ import * as Lifx from "node-lifx-lan";
 
 const off = async () => Lifx.turnOffBroadcast({});
 
-const on = async () => Lifx.turnOnBroadcast({ css: "white" });
+const on = async () => Lifx.turnOnBroadcast({ color: { css: "white" } });
 
 export default { on, off };
