5a094b8cb8e0d232d2e4bb5549c3d347f518e699
tvand <tvandort1@gmail.com>
2018-02-04 17:54:16 -0500

    Fix deprecation warning.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import * as express from "express";
+import { json, urlencoded } from "body-parser";
 import { check, validationResult } from "express-validator/check";
 import Lights from "./lights";
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(json());
+app.use(urlencoded());
 
 app.use("/lights", Lights);
 
